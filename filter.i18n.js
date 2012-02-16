0dab0634730936668723f9dc4b7d1490ab1931ad
Marco Michely <marco.michely@googlemail.com>
2012-02-16 20:55:58 +0100

    current state

--- filter.i18n.js
+++ filter.i18n.js
@@ -23,22 +23,52 @@ angular.filter('i18n', function(string) {
     var placeholders = [];
 
     for(var i=1; i < arguments.length; i++) {
-        placeholders.push(arguments[i]);
+        if(typeof(arguments[i]) == 'object') {
+            angular.forEach(arguments[i], function(item) {
+                placeholders.push(item);
+            })
+        }
+        else {
+            placeholders.push(arguments[i]);
+        }
     }
 
-    // Translation for strings
-    var translate = function(string, placeholders) {
-        var placeholders = placeholders || null;
-        var translated = lang[string]; // lang ist from the language file, e.g. de_DE.js
-        if (translated === undefined) {
-        /*if (log_untranslated == true) {
+    var translated = lang[string]; // lang ist from the language file, e.g. de_DE.js
+    if (translated === undefined) {
+        if (log_untranslated == true) {
             // here we could track unreanslated strings by sending them to the server...
-        }*/
-            return sprintf(string, placeholders);
         }
-        return sprintf(translated, placeholders);
-    };
+        return sprintf(string, placeholders);
+    }
+    return sprintf(translated, placeholders);
 
-    var translated = translate(string, placeholders);
-    return translated;
+});
+
+/**
+ * formats date _object_ to localized string by using date.js
+ * See http://www.datejs.com/
+ *
+ * Usage in template:
+ * Date only: {created_at|i18nDate}}
+ * Date and time: {created_at|i18nDate:true}}
+ *
+ * Usage in controller:
+ * angular.filter.i18nDate(self.created_at);
+ */
+angular.filter('i18nDate', function(date,time) {
+    var time = time || false;
+    if(typeof date === 'string') {
+        var date = Date.parse(date);
+    }
+
+    if(!date) {
+        return;
+    }
+
+    if(time) {
+        return date.toString(angular.filter.i18n('M/d/yyyy HH:mm'));
+    }
+    else {
+        return date.toString(angular.filter.i18n('M/d/yyyy'));
+    }
 });
--- widget.chosen.js
+++ widget.chosen.js
@@ -12,21 +12,29 @@
  *    my:chosen="availableContacts"></select>
  *
  */
-angular.widget('@my:chosen', function(expression,element) {
+angular.widget('my:chosen', function(expression,element) {
     function instanceFn(element) { // this is how we can inject services properly... (not needed here)
         var currentScope = this;
         var i18n = angular.filter.i18n;
         currentScope.$watch(expression, function(value) {
             if(value) {
+                var elemName = element.attr('name');
                 if(element.attr('data-placeholder'))
                 {
                     element.append('<option value="">' + i18n(element.attr('data-placeholder'))+ '</option>');
                 }
                 angular.forEach(value,function(item, index) {
-                    element.append('<option value="' + item + '">' + item  + '</option>')
+                    if(item.length > 0 && item != undefined) {
+                        var currentValue = eval('currentScope.' + elemName);
+                        if(item == currentValue) {
+                            element.append('<option value="' + item + '" selected>' + item  + '</option>')
+                        }
+                        else {
+                            element.append('<option value="' + item + '">' + item  + '</option>')
+                        }
+                    }
                 });
                 element.chosen({no_results_text: i18n("No results matched")}).change(function() {
-                    var elemName = element.attr('name');
                     currentScope[elemName] = element.val();
                 });
             }
